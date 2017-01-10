e9ca9e26f351777e268a4dcd1e299934ee9eeed9
ohgane <ohgane@me.comgit config --global user.name ohgane>
2017-01-10 09:28:34 +0900

    Add how to view vignette

--- R/DigestionSpecificity.R
+++ R/DigestionSpecificity.R
@@ -3,7 +3,7 @@
 #' This package provides a set of functions that allows to plot
 #' cleavage site specificity based on identified peptide sequences. Both single
 #' amino acid level plot (barchart) and dipeptide-level specificity (heatmap) can
-#' be plotted. For more detail, see the package vignette with \code{vignette(package="DigestionSpecificity")}.
+#' be plotted. For more detail, see the package vignette with \code{browseVignettes(package="DigestionSpecificity")}.
 #'
 #' @seealso \code{\link{plotCleavage}}, \code{\link{plotCleavageMatrix}}
 #'
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DigestionSpecificity
 An R package for analysis of cleavage site specificity (proteomics)
+
+To install this package, run the follwoings in R:
+    library(devtools)
+    install_github("ohgane/DigestionSpecificity")
+
+If you don't have `devtools` yet, install it first.
+    install.packages("devtools")
+
+After loading `DigestionSpecificity` package, you can check out a tutorial (package vignette) by:
+    browseVignettes(package="DigestionSpecificity")
